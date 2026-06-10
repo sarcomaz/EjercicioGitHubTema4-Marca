720522bf8b06e44b5dc98880218547cd7ff57fb5
sarcomaz <lehernandez1363@gmail.com>
2026-06-10 16:42:10 +0200

    Update Main.java

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
 	public static void main(String[] args) {
-System.out.println("Cambio realizado en rama conflicto");
+System.out.println("Cambio realizado en rama conflicto :)");
 	}
 }
